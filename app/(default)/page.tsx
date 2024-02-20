cd74d41690b4bec4341d4897c9b11cf7d9361213
cliffordmark.musni <cliffordmark.musni@finastra.com>
2024-02-21 02:53:51 +0800

    Added Flowbite and updated routing

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,9 +5,6 @@ export const metadata = {
 
 import Hero from "../../components/hero";
 import Branches from "../../components/branches";
-import FeaturesBlocks from "../../components/features-blocks";
-import Testimonials from "../../components/testimonials";
-import Newsletter from "../../components/newsletter";
 
 export default function Home() {
   return (
